20691ec4dc0db6b187039bcd49d522d2e3748345
samdogg7 <samdogg7@gmail.com>
2024-10-27 08:46:43 -0400

    Updated BottomSheetViewConfiguration to be Observable and added selectedDetent to ensure the detent height remains the same when swapping tabs. In addition, ensured sheetHeight does not go below the smallest detent. I noticed with List that I could shrink the BottomSheet size to 0 and not be able to reshow the sheet.

--- Sources/BottomSheet/BottomSheetView/BottomSheetViewConfiguration.swift
+++ Sources/BottomSheet/BottomSheetView/BottomSheetViewConfiguration.swift
@@ -7,20 +7,42 @@
 
 import SwiftUI
 
+@Observable
 class BottomSheetViewConfiguration {
-    var sheetColor: Color
+    var sheetColor: Color?
     var dragIndicator: DragIndicator
+    var selectedDetent: Detent
     var detents: [Detent]
+    var cornerRadius: CGFloat
+    /// Edges that ignore the safe area
     var ignoredEdges: Edge.Set?
     
-    init(sheetColor: Color = .white,
-         dragIndicator: DragIndicator = .init(),
-         detents: [Detent] = [.large],
-         ignoredEdges: Edge.Set? = .bottom) {
+    init(
+        sheetColor: Color? = nil,
+        dragIndicator: DragIndicator = .init(),
+        detents: [Detent] = [.large],
+        cornerRadius: CGFloat = 20,
+        ignoredEdges: Edge.Set? = .bottom
+    ) {
         self.sheetColor = sheetColor
         self.dragIndicator = dragIndicator
-        self.detents = detents
+        self.cornerRadius = cornerRadius
         self.ignoredEdges = ignoredEdges
+        
+        var detents = detents
+
+        // Ensure there is always a detent present in `detents`
+        if detents.isEmpty {
+            assertionFailure("`detents` should always be populated")
+            detents.append(.large)
+        }
+        self.detents = detents
+
+        guard let smallestDetent = detents.min(by: { $0.fraction < $1.fraction }) else {
+            preconditionFailure("`smallestDetent` should never be nil")
+        }
+
+        self.selectedDetent = smallestDetent
     }
 }
 
