d516d7add7e52cace92bf2d8a9cacda9ca5ed83d
Wojciech Konury <wojciechkonury@gmail.com>
2024-10-28 15:34:18 +0100

    Add documentation to inits

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -39,6 +39,10 @@ public struct BottomSheet<Content: View, SheetContent: View>: View {
 }
 
 public extension View {
+    /// Presents a sheet when a binding to a Boolean value that you provide is true.
+    /// - Parameters:
+    ///   - isPresented: A binding to a Boolean value that determines whether to present the sheet that you create in the modifier’s content closure.
+    ///   - sheetContent: A closure that returns the content of the sheet.
     func bottomSheet<SheetContent: View>(
         isPresented: Binding<Bool>,
         @ViewBuilder sheetContent: () -> SheetContent
@@ -50,6 +54,8 @@ public extension View {
         )
     }
     
+    /// Presents a sheet.
+    /// - Parameter sheetContent: A closure that returns the content of the sheet.
     func bottomSheet<SheetContent: View>(
         @ViewBuilder sheetContent: () -> SheetContent
     ) -> BottomSheet<Self, SheetContent> {
