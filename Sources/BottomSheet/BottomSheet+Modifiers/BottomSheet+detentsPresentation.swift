f7bff1bbd0fc3515054bdda7623b60adb1afbe16
Sam Doggett <samdogg7@gmail.com>
2024-10-28 08:06:06 -0400

    Updated setInitialDetent to true after setting initial detent height (#17)

--- Sources/BottomSheet/BottomSheet+Modifiers/BottomSheet+detentsPresentation.swift
+++ Sources/BottomSheet/BottomSheet+Modifiers/BottomSheet+detentsPresentation.swift
@@ -11,6 +11,8 @@ public extension BottomSheet {
     func detentsPresentation(initialDetent: Detent? = nil, detents: [Detent]) -> BottomSheet {
         configuration.detents = detents
         if let initialDetent = initialDetent ?? detents.smallest, !configuration.setInitialDetent {
+            // Ensure this conditional is executed once
+            configuration.setInitialDetent = true
             configuration.selectedDetent = initialDetent
         }
         return self
